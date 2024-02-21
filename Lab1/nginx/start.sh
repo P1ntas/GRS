462fd93ec1643a8b62185031054166842e2d4cae
Moreira <@gmail.com>
2024-02-21 12:27:52 +0000

    lab 1.2

--- Lab1/nginx/start.sh
+++ Lab1/nginx/start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 /sbin/ip route replace default via 10.0.2.254
+
+# Generate or modify the index.html file
+cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Info</title>
+</head>
+<body>
+    <h1>Hostname: $(hostname)</h1>
+</body>
+</html>
+EOF
+
+# Start Nginx in the foreground
 nginx -g "daemon off;"
