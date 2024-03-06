ec9ba18a8bfd03b53d744db0556e3e61029f16de
Francisco Pimentel Serra <franciscopimentelserra@gmail.com>
2024-03-06 12:09:48 +0000

    Organize folders but lab2-2 is complete shittt

--- Lab2/run_org1-ospf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://www.nongnu.org/quagga/docs/quagga.html
-export GORS_HOME=`pwd`
-$GORS_HOME/docker-prune.sh
-scp -r $GORS_HOME/quagga gors-target:~/.
-$GORS_HOME/runremote.sh gors-target $GORS_HOME/org1-ospf.sh
